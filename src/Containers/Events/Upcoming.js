cb4fd7338e05a88ea8ab6e9f4d03fbb5a94a198c
Phil <phillippelliott@gmail.com>
2021-12-04 06:26:27 -0500

    Fixed event cards

--- src/Containers/Events/Upcoming.js
+++ src/Containers/Events/Upcoming.js
@@ -1,12 +1,9 @@
 const Upcoming = () => {
   return (
     <div className="events-bottom">
-      <div
-        className="topMenuContainer container"
-        style={{ background: "#dbdbdb", color: "black" }}
-      >
-        <div className="menuTopLeft">
-          <h4>Friday's 7PM - 9PM</h4>
+      <div className="bottom-events container">
+        <div className="eventsTopLeft">
+          <h5>Friday's 7PM - 9PM</h5>
           <h2>Live Music</h2>
           <p>
             Nothing is better than live music after a long day. Come and enjoy
@@ -14,18 +11,17 @@ const Upcoming = () => {
           </p>
           <div className="buttons">
             <div className="button">
-              <button style={{ background: "#5d5d5f" }}>
-                <a style={{ color: "white" }} href="#">
-                  Reserve
-                </a>
+              <button>
+                <a style={{ color: "white" }}>Reserve</a>
               </button>
+              <p>2 Seats Left</p>
             </div>
           </div>
         </div>
-        <div className="menuTopRight">
+        <div className="eventsTopRight">
           <img
             className="imageContain"
-            src="https://images.unsplash.com/photo-1497935586351-b67a49e012bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+            src="https://images.unsplash.com/photo-1510915361894-db8b60106cb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt="Coffee"
           />
         </div>
