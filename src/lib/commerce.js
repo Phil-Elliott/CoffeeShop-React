670501cd79f7482aa05b54b8e6a1b7e2489ee490
Phil <phillippelliott@gmail.com>
2022-01-01 15:41:20 -0500

    deploy

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,5 +1,6 @@
 import Commerce from "@chec/commerce.js"
 
 export const commerce = new Commerce(
-  "pk_test_37223e261f675080b9ebf38c6ff6fa8cca0abcce66fba"
+  process.env.REACT_APP_CHECK_PUBLIC_KEY,
+  true
 )
