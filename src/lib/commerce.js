77857697a59815eb01c0c58af2d9c64bc4658962
Phil <phillippelliott@gmail.com>
2022-01-01 15:23:36 -0500

    deploy

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,6 +1,6 @@
 import Commerce from "@chec/commerce.js"
 
 export const commerce = new Commerce(
-  "pk_test_37223e261f675080b9ebf38c6ff6",
+  "pk_test_37223e261f675080b9ebf38c6ff6fa8cca0abcce66fba",
   true
 )
